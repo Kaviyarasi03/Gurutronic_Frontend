c8262dda3c642d965ff1cce86010f617d78fb07c
Kaviyarasi03 <kaviyarasi1603@gmail.com>
2026-08-25 22:35:44 +0530

    Add Vercel routing fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../src/components/Header";
+import Header from "../components/Header";
 
 import aboutShowroom from "../assets/about_bg.png";
 import customer1 from "../assets/customer1.png";
